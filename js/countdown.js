f853c41d6da6820dcb91ce9a4b601ee7f83927f7
Axel Derail Santer <115737371+siryo-ax@users.noreply.github.com>
2023-02-14 00:20:09 +0100

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,13 @@
+var totkOut = new Date("May 12, 2023 00:00:00").getTime();
+
+var now = new Date().getTime();
+
+var wait = totkOut-now;
+
+var days = Math.floor(wait/(1000*60*60*24));
+var hours = Math.floor((wait%(1000*60*60*24))/(1000*60*60));
+var mins = Math.floor((wait%(1000*60*60))/(1000*60));
+var secs = Math.floor((wait%(1000*60))/(1000));
+
+document.getElementById("cdown").textContent = days+" jours "+hours+" heures "+mins+" minutes "+secs+" secondes";
+
