3d17f9d7a97d4732b4e9f2ff84fa81fca9abef86
Axel Derail Santer <115737371+siryo-ax@users.noreply.github.com>
2023-05-09 19:56:58 +0200

    ajout audio

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,36 @@
-var totkOut = new Date("May 12, 2023 06:00:00").getTime();
+var totkOut = new Date("May 12, 2023 00:00:00").getTime();
+
+let days = document.getElementById("days");
+let hours = document.getElementById("hours");
+let mins = document.getElementById("mins");
+let secs = document.getElementById("secs");
+
+var isOutWrote = false; 
 
 function countdown() {
     var now = new Date().getTime();
-
     var wait = totkOut-now;
 
-    var days = Math.floor(wait/(1000*60*60*24));
-    var hours = Math.floor((wait%(1000*60*60*24))/(1000*60*60));
-    var mins = Math.floor((wait%(1000*60*60))/(1000*60));
-    var secs = Math.floor((wait%(1000*60))/(1000));
+    if(wait<0 && !isOutWrote){
+        let countdown = document.getElementById("countdown"); 
+        let isOut = document.createTextNode('Le jeu est sorti !');
+        countdown.prepend(isOut);
+        isOutWrote = true; 
+        days.remove(); 
+        hours.remove();
+        mins.remove();
+        secs.remove(); 
+    }else{
+        var tdays = Math.floor(wait/(1000*60*60*24));
+        var thours = Math.floor((wait%(1000*60*60*24))/(1000*60*60));
+        var tmins = Math.floor((wait%(1000*60*60))/(1000*60));
+        var tsecs = Math.floor((wait%(1000*60))/(1000));
 
-    document.getElementById("days").textContent = days+" jours ";
-    document.getElementById("hours").textContent = hours+" heures "
-    document.getElementById("mins").textContent = mins+" minutes ";
-    document.getElementById("secs").textContent = secs+" secondes";
+        days.textContent = tdays+" jours ";
+        hours.textContent = thours+" heures "
+        mins.textContent = tmins+" minutes ";
+        secs.textContent = tsecs+" secondes";
+    }
 }
 
 setInterval(countdown,1000);
