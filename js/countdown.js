190656519c4933097396408a087042628ba1cd34
Axel Derail Santer <115737371+siryo-ax@users.noreply.github.com>
2023-05-09 21:17:53 +0200

    gestion de la fin du compte à rebours

--- js/countdown.js
+++ js/countdown.js
@@ -11,26 +11,29 @@ function countdown() {
     var now = new Date().getTime();
     var wait = totkOut-now;
 
-    if(wait<0 && !isOutWrote){
-        let countdown = document.getElementById("countdown"); 
-        let isOut = document.createTextNode('Le jeu est sorti !');
-        countdown.prepend(isOut);
-        isOutWrote = true; 
-        days.remove(); 
-        hours.remove();
-        mins.remove();
-        secs.remove(); 
-    }else{
-        var tdays = Math.floor(wait/(1000*60*60*24));
-        var thours = Math.floor((wait%(1000*60*60*24))/(1000*60*60));
-        var tmins = Math.floor((wait%(1000*60*60))/(1000*60));
-        var tsecs = Math.floor((wait%(1000*60))/(1000));
+    var tdays = Math.floor(wait/(1000*60*60*24));
+    var thours = Math.floor((wait%(1000*60*60*24))/(1000*60*60));
+    var tmins = Math.floor((wait%(1000*60*60))/(1000*60));
+    var tsecs = Math.floor((wait%(1000*60))/(1000));
 
-        days.textContent = tdays+" jours ";
-        hours.textContent = thours+" heures "
-        mins.textContent = tmins+" minutes ";
-        secs.textContent = tsecs+" secondes";
-    }
+    days.textContent = tdays+" jours ";
+    hours.textContent = thours+" heures "
+    mins.textContent = tmins+" minutes ";
+    secs.textContent = tsecs+" secondes";
 }
 
-setInterval(countdown,1000);
+var now = new Date().getTime();
+var wait = totkOut-now;
+
+if(wait>0){
+    setInterval(countdown,1000);
+}else{
+    let countdown = document.getElementById("countdown"); 
+    let isOut = document.createTextNode('Le jeu est sorti !');
+    countdown.prepend(isOut);
+    isOutWrote = true; 
+    days.remove(); 
+    hours.remove();
+    mins.remove();
+    secs.remove();
+}
